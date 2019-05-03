efe02d71b652818d8005e003591fd64400447f9b
Cayman <caymannava@gmail.com>
2019-05-03 09:49:58 -0500

    Add buffer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+const x = Buffer.alloc(30)
+
+
 export default class extends React.Component {
   render() {
-    return <p>Babel + TypeScript + React = ❤️</p>;
+    return <p>Babel +wft TypeScript + React = ❤️  {x.toString('hex')}</p>;
   }
 }
